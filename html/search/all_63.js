b942860bede89cb0f3395c31147eb1e3ffd7a867
KSYong <ericyong95@gmail.com>
2020-07-20 18:00:34 +0900

    added information transfer by UDP in add function. doxygen changed.

--- html/search/all_63.js
+++ html/search/all_63.js
@@ -2,12 +2,14 @@ var searchData=
 [
   ['client_2ec',['client.c',['../client_8c.html',1,'']]],
   ['client_2eh',['client.h',['../client_8h.html',1,'']]],
-  ['client_5fadd_5fdata',['client_add_data',['../client_8c.html#a8cfe1f951153da310bba8c345fe2a98c',1,'client_add_data(client_t *client):&#160;client.c'],['../client_8h.html#a8cfe1f951153da310bba8c345fe2a98c',1,'client_add_data(client_t *client):&#160;client.c']]],
+  ['client_5fadd_5fdata',['client_add_data',['../client_8c.html#ac77a5cabe930e0194fab2f97ababb5eb',1,'client_add_data(client_t *client, char *title, char *author):&#160;client.c'],['../client_8h.html#ac77a5cabe930e0194fab2f97ababb5eb',1,'client_add_data(client_t *client, char *title, char *author):&#160;client.c']]],
   ['client_5faddr',['client_addr',['../structserver__s.html#a542d49f8aee930024091c320530d8dbf',1,'server_s']]],
   ['client_5fdestroy',['client_destroy',['../client_8c.html#a78c6ed6e74b3ce44da9950931b7d1381',1,'client_destroy(client_t *client):&#160;client.c'],['../client_8h.html#a78c6ed6e74b3ce44da9950931b7d1381',1,'client_destroy(client_t *client):&#160;client.c']]],
   ['client_5fdisplay_5fdata',['client_display_data',['../client_8c.html#a075167f6395030489e0a63815bb26050',1,'client_display_data(client_t *client):&#160;client.c'],['../client_8h.html#a075167f6395030489e0a63815bb26050',1,'client_display_data(client_t *client):&#160;client.c']]],
   ['client_5finit',['client_init',['../client_8c.html#a79115ea669ab14277ada1fc054ad01bf',1,'client_init():&#160;client.c'],['../client_8h.html#af21475138048fb4203362940473e97ee',1,'client_init():&#160;client.c']]],
   ['client_5fport',['CLIENT_PORT',['../server_8h.html#a653e745f64132435aea89e460c4d460d',1,'CLIENT_PORT():&#160;server.h'],['../client_8h.html#a653e745f64132435aea89e460c4d460d',1,'CLIENT_PORT():&#160;client.h']]],
+  ['client_5frecv_5fdata',['client_recv_data',['../client_8c.html#a5077e9f6991a792d9dd8fab413795d2a',1,'client_recv_data(client_t *client):&#160;client.c'],['../client_8h.html#a5077e9f6991a792d9dd8fab413795d2a',1,'client_recv_data(client_t *client):&#160;client.c']]],
   ['client_5fs',['client_s',['../structclient__s.html',1,'']]],
+  ['client_5fsend_5fdata',['client_send_data',['../client_8c.html#a9311d59bd2a73b2b5c9443ba42efc106',1,'client_send_data(client_t *client, char *send_buf):&#160;client.c'],['../client_8h.html#a9311d59bd2a73b2b5c9443ba42efc106',1,'client_send_data(client_t *client, char *send_buf):&#160;client.c']]],
   ['client_5ft',['client_t',['../structclient__t.html',1,'client_t'],['../client_8h.html#a7bfebc8e763f36d1870198931d6b5eb1',1,'client_t():&#160;client.h']]]
 ];
--- html/search/functions_63.js
+++ html/search/functions_63.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['client_5fadd_5fdata',['client_add_data',['../client_8c.html#a8cfe1f951153da310bba8c345fe2a98c',1,'client_add_data(client_t *client):&#160;client.c'],['../client_8h.html#a8cfe1f951153da310bba8c345fe2a98c',1,'client_add_data(client_t *client):&#160;client.c']]],
+  ['client_5fadd_5fdata',['client_add_data',['../client_8c.html#ac77a5cabe930e0194fab2f97ababb5eb',1,'client_add_data(client_t *client, char *title, char *author):&#160;client.c'],['../client_8h.html#ac77a5cabe930e0194fab2f97ababb5eb',1,'client_add_data(client_t *client, char *title, char *author):&#160;client.c']]],
   ['client_5fdestroy',['client_destroy',['../client_8c.html#a78c6ed6e74b3ce44da9950931b7d1381',1,'client_destroy(client_t *client):&#160;client.c'],['../client_8h.html#a78c6ed6e74b3ce44da9950931b7d1381',1,'client_destroy(client_t *client):&#160;client.c']]],
   ['client_5fdisplay_5fdata',['client_display_data',['../client_8c.html#a075167f6395030489e0a63815bb26050',1,'client_display_data(client_t *client):&#160;client.c'],['../client_8h.html#a075167f6395030489e0a63815bb26050',1,'client_display_data(client_t *client):&#160;client.c']]],
-  ['client_5finit',['client_init',['../client_8c.html#a79115ea669ab14277ada1fc054ad01bf',1,'client_init():&#160;client.c'],['../client_8h.html#af21475138048fb4203362940473e97ee',1,'client_init():&#160;client.c']]]
+  ['client_5finit',['client_init',['../client_8c.html#a79115ea669ab14277ada1fc054ad01bf',1,'client_init():&#160;client.c'],['../client_8h.html#af21475138048fb4203362940473e97ee',1,'client_init():&#160;client.c']]],
+  ['client_5frecv_5fdata',['client_recv_data',['../client_8c.html#a5077e9f6991a792d9dd8fab413795d2a',1,'client_recv_data(client_t *client):&#160;client.c'],['../client_8h.html#a5077e9f6991a792d9dd8fab413795d2a',1,'client_recv_data(client_t *client):&#160;client.c']]],
+  ['client_5fsend_5fdata',['client_send_data',['../client_8c.html#a9311d59bd2a73b2b5c9443ba42efc106',1,'client_send_data(client_t *client, char *send_buf):&#160;client.c'],['../client_8h.html#a9311d59bd2a73b2b5c9443ba42efc106',1,'client_send_data(client_t *client, char *send_buf):&#160;client.c']]]
 ];
